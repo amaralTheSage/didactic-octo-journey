cf056cb084db462cf2d4996b42dc9397e96c9d3b
amaralTheSage <amaraldesouza9@gmail.com>
2025-12-06 09:30:51 -0300

    Adds agency logos and influencer avatars with temporary values

--- app/Filament/Resources/Agencies/Tables/AgenciesTable.php
+++ app/Filament/Resources/Agencies/Tables/AgenciesTable.php
@@ -5,8 +5,10 @@ namespace App\Filament\Resources\Agencies\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Schemas\Components\Image;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
@@ -17,6 +19,11 @@ class AgenciesTable
     {
         return $table
             ->columns([
+                ImageColumn::make('logo')
+                    ->label('')
+                    ->circular()
+                    ->default('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fd1a3f4spazzrp4.cloudfront.net%2Fuber-com%2F1.1.8%2Fd1a3f4spazzrp4.cloudfront.net%2Fimages%2Ffacebook-shareimage-1-c3462391c9.jpg&f=1&nofb=1&ipt=2d5599fe5390a3a3ae3cf9936a3ef2fd2a1babb90ef970f18fb474e888b2fb90'),
+
                 TextColumn::make('name')->label('Nome')
                     ->searchable(),
 
--- app/Filament/Resources/Influencers/Tables/InfluencersTable.php
+++ app/Filament/Resources/Influencers/Tables/InfluencersTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Influencers\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -14,6 +15,10 @@ class InfluencersTable
     {
         return $table
             ->columns([
+                ImageColumn::make('avatar')
+                    ->label('')
+                    ->circular()
+                    ->default('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fogimg.infoglobo.com.br%2Feconomia%2F25035438-701-611%2FFT1086A%2F92729986.jpg&f=1&nofb=1&ipt=251fab699105e037d167e46c1f5264a920953b961a9d7f902f7706593b8f7c79'),
                 TextColumn::make('name')->label('Nome')
                     ->searchable(),
                 TextColumn::make('agency.name')->label('Agência')->default('___')
