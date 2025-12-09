ae2b351be5d12fb5d8727fe4bcc48d752598ff97
amaralTheSage <amaraldesouza9@gmail.com>
2025-12-09 10:12:20 -0300

    updates seeder to add several users and use placeholder images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,51 +2,120 @@
 
 namespace Database\Seeders;
 
-use App\Models\Test;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Filament\Support\Colors\Color;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-        User::create(
-            [
-                'name' => 'Uber',
-                'email' => 'empresa@gmail.com',
-                'avatar' => null,
-                'password' => Hash::make('senha123'),
-                'role' => 'company',
-                'email_verified_at' => now(),
-            ]
-        );
+        Storage::disk('public')->makeDirectory('avatars');
 
-        User::create(
-            [
-                'name' => 'DSTA Agência',
-                'email' => 'agencia@gmail.com',
-                'avatar' => null,
-                'password' => Hash::make('senha123'),
-                'role' => 'agency',
-                'email_verified_at' => now(),
-            ]
-        );
 
-        User::create(
-            [
-                'name' => 'Influencer X',
-                'email' => 'influencer@gmail.com',
-                'avatar' => null,
+        $createAvatar = function () {
+            $filename = Str::random(20) . '.jpg';
+
+            $url = 'https://picsum.photos/300/300?random=' . Str::random(10);
+
+            $imageData = file_get_contents($url);
+
+            Storage::disk('public')->put("avatars/{$filename}", $imageData);
+
+            return "avatars/{$filename}";
+        };
+
+
+        // -------------------------------------------------------
+        // COMPANIES
+        // -------------------------------------------------------
+        $companies = collect();
+        foreach (range(1, 10) as $i) {
+            $companies->push(
+                User::create([
+                    'name' => "Company $i",
+                    'email' => "company$i@gmail.com",
+                    'avatar' => $createAvatar("Company $i"),
+                    'password' => Hash::make('senha123'),
+                    'role' => 'company',
+                    'email_verified_at' => now(),
+                ])
+            );
+        }
+
+        // -------------------------------------------------------
+        // AGENCIES
+        // -------------------------------------------------------
+        $agencies = collect();
+        foreach (range(1, 10) as $i) {
+            $agencies->push(
+                User::create([
+                    'name' => "Agency $i",
+                    'email' => "agency$i@gmail.com",
+                    'avatar' => $createAvatar("Agency $i"),
+                    'password' => Hash::make('senha123'),
+                    'role' => 'agency',
+                    'email_verified_at' => now(),
+                ])
+            );
+        }
+
+        // -------------------------------------------------------
+        // INFLUENCERS
+        // -------------------------------------------------------
+        foreach (range(1, 30) as $i) {
+            User::create([
+                'name' => "Influencer $i",
+                'email' => "influencer$i@gmail.com",
+                'avatar' => $createAvatar("Influencer $i"),
                 'password' => Hash::make('senha123'),
                 'role' => 'influencer',
+                'agency_id' => $agencies->random()->id,
                 'email_verified_at' => now(),
-            ]
-        );
+            ]);
+        }
+
+        // -------------------------------------------------------
+        // -------------------------------------------------------
+        User::create([
+            'name' => 'Empresa A',
+            'email' => 'empresa@gmail.com',
+            'avatar' => $createAvatar("Empresa A"),
+            'password' => Hash::make('senha123'),
+            'role' => 'company',
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Agência A',
+            'email' => 'agencia@gmail.com',
+            'avatar' => $createAvatar("Agência A"),
+            'password' => Hash::make('senha123'),
+            'role' => 'agency',
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Influencer A1',
+            'email' => 'influencer@gmail.com',
+            'avatar' => $createAvatar("Influencer A1"),
+            'password' => Hash::make('senha123'),
+            'role' => 'influencer',
+            'agency_id' => 2,
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Influencer A2',
+            'email' => 'influencera2@gmail.com',
+            'avatar' => $createAvatar("Influencer A2"),
+            'password' => Hash::make('senha123'),
+            'role' => 'influencer',
+            'agency_id' => 2,
+            'email_verified_at' => now(),
+        ]);
     }
 }
